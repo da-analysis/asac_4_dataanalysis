5819e6a6993bd6e3d60967ab607852cd83cf2d44
JIYUN0710 <JIYUN0710>
2024-03-26 07:04:01 +0000

    #2 meta_final
    delta 형식으로 저장

--- analysis/0326_meta_final.py
+++ analysis/0326_meta_final.py
@@ -17,25 +17,28 @@ import pyspark.pandas as ps
 
 cell_phones_and_accessories = sc.textFile("dbfs:/FileStore/amazon/metadata/meta_Cell_Phones_and_Accessories.json")
 
+sports_and_outdoors = sc.textFile('dbfs:/FileStore/amazon/metadata/meta_Sports_and_Outdoors.json')
+
 # COMMAND ----------
 
 cell_phones_and_accessories.getNumPartitions()
+sports_and_outdoors.getNumPartitions()
 
 # COMMAND ----------
 
 cell_phones_and_accessories.coalesce(4).getNumPartitions()
+sports_and_outdoors.coalesce(4).getNumPartitions()
 
 # COMMAND ----------
 
 cell_phones_and_accessories.coalesce(4).saveAsTextFile("dbfs:/FileStore/amazon/metadata/v2/meta_Cell_Phones_and_Accessories_v2")
-
-# COMMAND ----------
-
-display(dbutils.fs.ls('dbfs:/FileStore/amazon/metadata/v2/meta_Cell_Phones_and_Accessories_v2/'))
+sports_and_outdoors.coalesce(4).saveAsTextFile("dbfs:/FileStore/amazon/metadata/v2/meta_sports_and_outdoors")
 
 # COMMAND ----------
 
 meta_Cell_v2 = ps.read_json("/FileStore/amazon/metadata/v2/meta_Cell_Phones_and_Accessories_v2/*", lines=True)
+
+sports_and_outdoors = ps.read_json("dbfs:/FileStore/amazon/metadata/v2/meta_sports_and_outdoors/*", lines=True)
 
 # COMMAND ----------
 
@@ -63,6 +66,8 @@ display(meta_Cell_v2)
 
 meta_Cell_v2 = meta_Cell_v2.drop(columns=["details", "fit", "imageURL", "main_cat", "tech1", "tech2"])
 
+sports_and_outdoors = sports_and_outdoors.drop(columns=["details", "fit", "imageURL", "main_cat", "tech1", "tech2"])
+
 # COMMAND ----------
 
 
@@ -77,23 +82,6 @@ meta_Cell_v2 = meta_Cell_v2.drop(columns=["details", "fit", "imageURL", "main_ca
 
 # COMMAND ----------
 
-meta_Cell_v2['also_buy'] = meta_Cell_v2['also_buy'].apply(lambda x: None if x.size == 0 else x)
-meta_Cell_v2['also_view'] = meta_Cell_v2['also_view'].apply(lambda x: None if x.size == 0 else x)
-meta_Cell_v2['description'] = meta_Cell_v2['description'].apply(lambda x: None if x.size == 0 else x)
-meta_Cell_v2['feature'] = meta_Cell_v2['feature'].apply(lambda x: None if x.size == 0 else x)
-meta_Cell_v2['imageURLHighRes'] = meta_Cell_v2['imageURLHighRes'].apply(lambda x: None if x.size == 0 else x)
-
-# COMMAND ----------
-
-meta_Cell_v2['brand'] = meta_Cell_v2['brand'].apply(lambda x: None if x=="" or x == "Unknown" else x)
-
-# COMMAND ----------
-
-meta_Cell_v2['similar_item'] = meta_Cell_v2['similar_item'].apply(lambda x: None if x is None or len(x) == 0 else x)
-meta_Cell_v2['title'] = meta_Cell_v2['title'].apply(lambda x: None if x is None or len(x) == 0 else x)
-
-# COMMAND ----------
-
 # []를 null로 바꾸는 함수
 import pandas as pd
 import numpy as np
@@ -105,8 +93,18 @@ def make_null_list(x):
     return None if x.size == 0 else x
 
 
-# 함수 적용 예시
-# meta_Cell_v2['also_buy'] = meta_Cell_v2['also_buy'].apply(make_null)
+# 함수 적용
+meta_Cell_v2['also_buy'] = meta_Cell_v2['also_buy'].apply(make_null_list)
+meta_Cell_v2['also_view'] = meta_Cell_v2['also_view'].apply(make_null_list)
+meta_Cell_v2['description'] = meta_Cell_v2['description'].apply(make_null_list)
+meta_Cell_v2['feature'] = meta_Cell_v2['feature'].apply(make_null_list)
+meta_Cell_v2['imageURLHighRes'] = meta_Cell_v2['imageURLHighRes'].apply(make_null_list)
+
+sports_and_outdoors['also_buy'] = sports_and_outdoors['also_buy'].apply(make_null_list)
+sports_and_outdoors['also_view'] = sports_and_outdoors['also_view'].apply(make_null_list)
+sports_and_outdoors['description'] = sports_and_outdoors['description'].apply(make_null_list)
+sports_and_outdoors['feature'] = sports_and_outdoors['feature'].apply(make_null_list)
+sports_and_outdoors['imageURLHighRes'] = sports_and_outdoors['imageURLHighRes'].apply(make_null_list)
 
 # COMMAND ----------
 
@@ -118,8 +116,10 @@ def make_null_brand(x):
     return None if x == "" or x == "Unknown" else x
 
 
-# 함수 적용 예시
-# meta_Cell_v2['brand'] = meta_Cell_v2['brand'].apply(make_null_brand)
+# 함수
+meta_Cell_v2['brand'] = meta_Cell_v2['brand'].apply(make_null_brand)
+
+sports_and_outdoors['brand'] = sports_and_outdoors['brand'].apply(make_null_brand)
 
 # COMMAND ----------
 
@@ -131,9 +131,12 @@ def make_null_blank(x):
     """
     return None if x is None or len(x) == 0 else x
 
-# 함수 적용 에시
-# meta_Cell_v2['similar_item'] = meta_Cell_v2['similar_item'].apply(make_null_blank)
+# 함수
+meta_Cell_v2['similar_item'] = meta_Cell_v2['similar_item'].apply(make_null_blank)
+meta_Cell_v2['title'] = meta_Cell_v2['title'].apply(make_null_blank)
 
+sports_and_outdoors['similar_item'] = sports_and_outdoors['similar_item'].apply(make_null_blank)
+sports_and_outdoors['title'] = sports_and_outdoors['title'].apply(make_null_blank)
 
 # COMMAND ----------
 
@@ -145,15 +148,6 @@ def make_null_blank(x):
 
 # COMMAND ----------
 
-meta_Cell_v2['cat1'] = meta_Cell_v2['category'].apply(lambda x: x[0] if len(x) >= 1 else None)
-meta_Cell_v2['cat2'] = meta_Cell_v2['category'].apply(lambda x: x[1] if len(x) >= 2 else None)
-meta_Cell_v2['cat3'] = meta_Cell_v2['category'].apply(lambda x: x[2] if len(x) >= 3 else None)
-meta_Cell_v2['cat4'] = meta_Cell_v2['category'].apply(lambda x: x[3] if len(x) >= 4 else None)
-
-display(meta_Cell_v2)
-
-# COMMAND ----------
-
 def extract_category(category, index):
     """
     category에서 지정된 인덱스에 해당하는 카테고리를 추출하고, 인덱스가 범위를 벗어나면 None을 반환
@@ -161,8 +155,36 @@ def extract_category(category, index):
     return category[index] if index < len(category) else None
 
 # 함수 적용 예시
-# meta_Cell_v2['cat1'] = meta_Cell_v2['category'].apply(lambda x: extract_category(x, 0))
-# meta_Cell_v2['cat2'] = meta_Cell_v2['category'].apply(lambda x: extract_category(x, 1))
+meta_Cell_v2['cat1'] = meta_Cell_v2['category'].apply(lambda x: extract_category(x, 0))
+meta_Cell_v2['cat2'] = meta_Cell_v2['category'].apply(lambda x: extract_category(x, 1))
+meta_Cell_v2['cat3'] = meta_Cell_v2['category'].apply(lambda x: extract_category(x, 2))
+meta_Cell_v2['cat4'] = meta_Cell_v2['category'].apply(lambda x: extract_category(x, 3))
+
+sports_and_outdoors['cat1'] = sports_and_outdoors['category'].apply(lambda x: extract_category(x, 0))
+sports_and_outdoors['cat2'] = sports_and_outdoors['category'].apply(lambda x: extract_category(x, 1))
+sports_and_outdoors['cat3'] = sports_and_outdoors['category'].apply(lambda x: extract_category(x, 2))
+sports_and_outdoors['cat4'] = sports_and_outdoors['category'].apply(lambda x: extract_category(x, 3))
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### asin 중복값 삭제
+
+# COMMAND ----------
+
+def remove_duplicate_rows(dataframe):
+    columns_to_drop_duplicate = ['asin']
+    dataframe = dataframe.drop_duplicates(subset=columns_to_drop_duplicate, keep='first')
+    return dataframe
+
+meta_Cell_v2 = remove_duplicate_rows(meta_Cell_v2)
+sports_and_outdoors = remove_duplicate_rows(sports_and_outdoors)
+
+# COMMAND ----------
 
 
 
@@ -182,13 +204,13 @@ def extract_category(category, index):
 
 meta_Cell_v2['date'] = ps.to_datetime(meta_Cell_v2['date'], errors='coerce')
 
-# COMMAND ----------
-
-display(meta_Cell_v2)
+sports_and_outdoors['date'] = ps.to_datetime(sports_and_outdoors['date'], errors='coerce')
 
 # COMMAND ----------
 
 meta_Cell_v2['date'] = meta_Cell_v2['date'].dt.strftime('%Y-%m-%d')
+
+sports_and_outdoors['date'] = sports_and_outdoors['date'].dt.strftime('%Y-%m-%d')
 
 # COMMAND ----------
 
@@ -230,9 +252,27 @@ def extract_price(price_str):
 
 meta_Cell_v2['new_price'] = meta_Cell_v2['price'].apply(extract_price)
 
+sports_and_outdoors['new_price'] = sports_and_outdoors['price'].apply(extract_price)
+
 # COMMAND ----------
 
 
+
+# COMMAND ----------
+
+meta_Cell_v2.info()
+
+# COMMAND ----------
+
+sports_and_outdoors.info()
+
+# COMMAND ----------
+
+display(meta_Cell_v2.head(100))
+
+# COMMAND ----------
+
+display(sports_and_outdoors.head(100))
 
 # COMMAND ----------
 
@@ -273,23 +313,21 @@ display(meta_Cell_v2)
 # COMMAND ----------
 
 meta_Cell_v2_fin=meta_Cell_v2
+sports_and_outdoors_fin=sports_and_outdoors
 
 # COMMAND ----------
 
-path = "dbfs:/FileStore/amazon/metadata/v2/meta_Cell_Phones_and_Accessories_v2/"
+path1 = "dbfs:/FileStore/amazon/metadata/v3/meta_Cell_Phones_and_Accessories_v3/"
+path2 = "dbfs:/FileStore/amazon/metadata/v3/sports_and_outdoors_v3/"
 
 # COMMAND ----------
 
-meta_Cell_v2_fin.to_parquet('%s/to_parquet/meta_Cell_v2.parquet' % path, compression='zstd')
+meta_Cell_v2_fin.to_parquet('%s/to_parquet/meta_Cell_v3.parquet' % path1, compression='zstd')
+sports_and_outdoors_fin.to_parquet('%s/to_parquet/sports_and_outdoors_v3.parquet' % path2, compression='zstd')
 
 # COMMAND ----------
 
 
-
-# COMMAND ----------
-
-# MAGIC %md
-# MAGIC DELTA 로 만들기
 
 # COMMAND ----------
 
@@ -310,25 +348,30 @@ spark = SparkSession.builder \
     .getOrCreate()
 
 # Parquet 파일 불러오기
-path = "dbfs:/FileStore/amazon/metadata/v2/meta_Cell_Phones_and_Accessories_v2/to_parquet/meta_Cell_v2.parquet"
+path = "dbfs:/FileStore/amazon/metadata/v3/meta_Cell_Phones_and_Accessories_v3/to_parquet/meta_Cell_v3.parquet"
 df = spark.read.parquet(path)
 
 display(df)
 
 # COMMAND ----------
 
-# Load the data from its source.
-df = spark.read.parquet("dbfs:/FileStore/amazon/metadata/v2/meta_Cell_Phones_and_Accessories_v2/to_parquet/meta_Cell_v2.parquet")
+path2 = "dbfs:/FileStore/amazon/metadata/v3/sports_and_outdoors_v3/to_parquet/sports_and_outdoors_v3.parquet"
+df2 = spark.read.parquet(path)
 
 # COMMAND ----------
 
-display(df)
+display(df2)
 
 # COMMAND ----------
 
 # Write the data to a table.
-table_name = "asac.meta_Cell_Phones_and_Accessories_v2"
+table_name = "asac.meta_Cell_Phones_and_Accessories_fin"
 df.write.saveAsTable(table_name)
+
+# COMMAND ----------
+
+table_name1 = "asac.sports_and_outdoors_fin"
+df2.write.saveAsTable(table_name1)
 
 # COMMAND ----------
 
